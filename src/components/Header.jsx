f3059cbd756493706715460688a1870c3fac4a07
SohamPethkar7038 <sohampethkar04@gmail.com>
2026-01-06 23:33:25 +0530

    add header component section

---
+++ src/components/Header.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+
+const Header = () => {
+  return (
+    <div className='flex items-center justify-between max-sm:flex-col-reverse gap-y-10 px-4 mt-10 lg:px-44 sm:mt-20'>
+
+        {/* ----left side----- */}
+        <div>
+            <h1 className=' text-4xl xl:text-5xl 2xl:text-6xl font-bold ml-3.9 text-neutral-700 leading-tight'>Remove the <br className='max-md:hidden' /> <span className='bg-linear-to-r from-violet-500 to bg-fuchsia-500 bg-clip-text text-transparent'>background</span> from <br className='max-md:hidden' /> images for free</h1>
+            <p className='my-4 ml-2.2 text-[15px] text-gray-500'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. <br className='max-sm:hidden' />Esse animi error eaque voluptate, voluptas vel?</p>
+            <div>
+                <input type="file" name='' id='upload1' hidden/>
+                <label className='inline-flex gap-3 px-8 py-3.5 rounded-full cursor-pointer bg-linear-to-r from-violet-600 to-fuchsia-500 m-auto hover:scale-105 transition-all duration-700' htmlFor="upload1">
+                    <img width={20} height={8} src={assets.upload_btn_icon} alt="" />
+                    <p className='text-white text-sm'>Upload your Image</p>
+                </label>
+            </div>
+        </div>
+        {/* ---right side */}
+        <div className='w-full max-w-md' >
+            <img width={380} className='lg:ml-24 sm:w-90 h-90 ' src={assets.header_img} alt="" />
+        </div>
+    </div>
+  )
+}
+
+export default Header
