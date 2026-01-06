4fa4dca00ef274f89e20e5ebe6cafe82af6356e7
SohamPethkar7038 <sohampethkar04@gmail.com>
2026-01-06 20:03:51 +0530

    add routes in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 
+import {Routes, Route ,Router, Navigate} from 'react-router-dom'
+import Home from './pages/Home.jsx'
+import BuyCredit from './pages/BuyCredit.jsx'
+import Result from './pages/Result.jsx'
 
 function App() {
   return (
-    <div className="text-2xl text-red-500 justifty-center ">
-      hello
-    </div>
+   <div>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/result' element={<Result/>}/>
+        <Route path='/buy' element={<BuyCredit/>}/>
+      </Routes>
+   </div>
   )
 }
 
