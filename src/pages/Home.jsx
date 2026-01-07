3f7e4774d82c8bba18967f5866f3332a5c93cf52
SohamPethkar7038 <sohampethkar04@gmail.com>
2026-01-07 08:40:22 +0530

    slider component section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from '../components/Header.jsx'
 import Steps from '../components/Steps.jsx'
+import BgSlide from '../components/BgSlide.jsx'
 
 const Home = () => {
   return (
     <div>
       <Header/>
       <Steps/>
+      <BgSlide/>
     </div>
   )
 }
