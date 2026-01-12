af610fc933534f04fb941252111c8ab26e79e722
SohamPethkar7038 <sohampethkar04@gmail.com>
2026-01-12 09:20:05 +0530

    add result component section

--- src/pages/Result.jsx
+++ src/pages/Result.jsx
@@ -5,25 +5,37 @@ import { assets } from '../assets/assets'
 const Result = () => {
   return (
     <div className='mx-4 my-3 lg:mx-44 mt-14 min-h-[75vh]'>
-    <div>
+    <div className='bg-white rounded-lg px-8 py-6 drop-shadow-sm'>
       {/*--image container---*/}
 
-      <div>
+      <div className='grid grid-cols-1 sm:grid-cols-2 gap-8 items-start'>
         {/* left side */}
 
         <div>
-          <p>Orignal</p>
-          <img src={assets.image_w_bg} alt="" />
+          <p className='font-semibold text-gray-600 mb-2'>Orignal</p>
+          <img className='rounded-md border' src={assets.image_w_bg} alt="" />
         </div>
 
         {/* right side */}
 
-        <div>
-          <p>Background remove</p>
-          <div>
+        <div className='grid grid-cols-1'>
+          <p className='font-semibold text-gray-600 mb-2'>Background remove</p>
+          <div className='rounded-md border border-gray-300 min-h-53  relative bg-layer overflow-hidden'>
             <img src={assets.image_wo_bg} alt="" />
-          </div>
+            {/* <div className='absolute right-1/2 bottom-1/2 transform translate-x-1/2 translate-y-1/2 '>
+            <div className='border-4 border-violet-600 rounded-full h-12 w-12 border-t-transparent animate-spin'> */}
+
+            {/* </div>
+          </div> */}
         </div>
+        </div>
+      </div>
+
+      {/* --buttons */}
+
+      <div className='flex justify-center sm:justify-center items-center flex-wrap gap-4 mt-4'>
+        <button className='px-8 py-2.5 text-violet-600 text-sm border border-violet-600 rounded-full hover:scale-105 transition-all duration-700 '>Try another image</button>
+        <a className='px-8 py-2.5 text-white text-sm bg-linear-to-r from-violet-600 to-fuchsia-500 rounded-full hover:scale-105 transition-all duration-700' href="">Download image</a>
       </div>
     </div>
     </div>
