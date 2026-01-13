fc67a3297fed2d8fc003ba80d5cf72019af79813
SohamPethkar7038 <sohampethkar04@gmail.com>
2026-01-13 14:46:21 +0530

    add buycredit section

--- frontend/src/pages/BuyCredit.jsx
+++ frontend/src/pages/BuyCredit.jsx
@@ -6,19 +6,19 @@ const BuyCredit = () => {
   return (
     <div className='min-h-[80vh] text-center pt-14 mb-10'>
       <button className='border border-gray-400 px-10 py-2 rounded-full mb-6'>Our Plans</button>
-      <h1 className='text-center text-2xl md:text-3xl lg:text-4xl mt-4 font-semibold bg-linear-to-r from-gray-900 to-gray-400 bg-clip-text text-transparent mb-6 sm:mb-10'>Choose the plan that's right for you</h1>
+      <h1 className='text-center text-2xl md:text-3xl lg:text-3xl mt-3.5 font-semibold bg-linear-to-r from-gray-900 to-gray-400 bg-clip-text text-transparent mb-6 sm:mb-10'>Choose the plan that's right for you</h1>
 
       {/* plans */}
       <div className='flex flex-wrap justify-center gap-6 text-left'>
         {plans.map((item,index)=>(
           <div className='bg-white drop-shadow-sm border rounded-lg py-12 px-8 text-gray-700 border-none hover:scale-105 transition-all duration-500' key={index}>
             <img width={40} src={assets.logo_icon} alt="" />
-            <p>{item.id}</p>
-            <p>{item.desc}</p>
-            <p>
-              <span>${item.price}</span>/ {item.credits} credits
+            <p className='nt-3 font-semibold'>{item.id}</p>
+            <p className='text-sm'>{item.desc}</p>
+            <p className='mt-6'>
+              <span className='text-3xl font-medium'>${item.price}</span>/ {item.credits} credits
               </p>
-              <button>Purchasef</button>
+              <button className='w-full bg-gray-800 text-white mt-8 text-sm rounded-md py-2 min-w-45'>Purchasef</button>
           </div>
         ))}
       </div>
