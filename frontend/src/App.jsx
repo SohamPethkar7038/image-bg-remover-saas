286bb6809bf2675814b8b3fac577f7d3dd5bb4b2
SohamPethkar7038 <sohampethkar04@gmail.com>
2026-01-13 15:28:39 +0530

    add user logic after signin/signup in navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,11 +5,11 @@ import BuyCredit from './pages/BuyCredit.jsx'
 import Result from './pages/Result.jsx'
 import Navbar from './components/Navbar.jsx'
 import Footer from './components/Footer.jsx'
+import { SignInButton } from '@clerk/clerk-react'
 function App() {
   return (
    <div>
      <Navbar/>
-    
       <Routes>
        
         <Route path='/' element={<Home/>}/>
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -3,15 +3,27 @@ import { Link } from "react-router-dom";
 
 
 import {assets} from '../assets/assets.js';
+import { useClerk, UserButton, useUser } from "@clerk/clerk-react";
 
 
 const Navbar=()=>{
+
+    const {openSignIn}=useClerk()
+    const {isSignedIn,user}=useUser()
+
     return(
         <div className="flex item-center justify-between mx-4 py-3 mt-3 lg:mx-25 ">
            <Link to='/'><img className="w-32 sm:w-44" src={assets.logo} alt="" /></Link> 
-            <button className="bg-zinc-800 text-white flex item-center gap-4 px-4 py-2 sm:py-3 text-sm rounded-full">
+           {
+            isSignedIn 
+            ? <div>
+                <UserButton/>
+            </div> 
+            :  <button onClick={()=>openSignIn({})} className="bg-zinc-800 text-white flex item-center gap-4 px-4 py-2 sm:py-3 text-sm rounded-full">
                 Get Started <img className="w-3 sm:w-4" src={assets.arrow_icon} alt="" />
             </button>
+           }
+           
         </div>
     ) 
 }
